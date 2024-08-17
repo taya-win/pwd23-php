3d41ab7197da5f70a33cb03d0c64503f8ba7e072
taya-win <tayawin.tyw@gmail.com>
2024-08-17 22:04:42 +0630

    Chapter(37) - PHP File Upload

--- profile.php
+++ profile.php
@@ -17,6 +17,29 @@ if(!isset($_SESSION['user'])){
 <body>
     <div class="container-xl mt-5">
         <h1 class="mb-3">John Doe (Manager)</h1>
+
+        <?php if(isset($_GET['error'])): ?>
+            <div class="alert alert-warning">
+                Cannot upload file
+            </div>
+        <?php endif ?>
+
+        <?php if(file_exists('_actions/photos/profile.jpg')): ?>
+            <img 
+                class="img-thumbnail mb-3"
+                src="_actions/photos/profile.jpg"
+                alt="Profile Photo" width="200"
+            >
+        <?php endif ?>
+
+        <form action="_actions/upload.php" method="post" 
+            enctype="multipart/form-data">
+            <div class="input-group mb-3">
+                <input type="file" name="photo" class="form-control">
+                <button class="btn btn-secondary">Upload</button>
+            </div>
+        </form>
+        
         <ul class="list-group">
             <li class="list-group-item">
                 <b>Email:</b> john.doe@gmail.com
